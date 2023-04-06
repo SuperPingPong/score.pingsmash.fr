768907e2fd8275752fec89376d698a64d133822a
Aurelien Duboc <aurelienduboc96@gmail.com>
2023-04-06 18:17:42 +0200

    feat: manage player switch on click

--- frontend/html/js/custom.js
+++ frontend/html/js/custom.js
@@ -1,3 +1,86 @@
+function updatePlayerData(license) {
+  $.ajax({
+    url: "/api/player",
+    data: {
+      license_number: license,
+    },
+    type: "GET",
+    success: function(selected_player) {
+      const result = $('#result');
+      for (var prop in selected_player) {
+          if (Object.prototype.hasOwnProperty.call(selected_player, prop)) {
+            $('#result_' + prop).html(selected_player[prop]);
+          }
+      }
+      // result.html(JSON.stringify(selected_player, null, 2));
+      result.show();
+    }
+  });
+  $.ajax({
+    url: "/api/matchs",
+    data: {
+      license_number: license,
+    },
+    type: "GET",
+    success: function(matchs_player) {
+
+      // const result = $('#result');
+      const matchs = $('#matchs');
+      let colCount = 0;
+      let rowDiv;
+
+      matchs_player.list.forEach((block, index_block) => {
+
+        block.journees.forEach((tournament) => {
+
+          // create a new row div after every 2 col-sm
+          if (colCount % 2 == 0) {
+            rowDiv = $('<div class="row"></div>');
+            matchs.append(rowDiv);
+          }
+
+          const colSm = $('<div class="col-sm-6"><hr><span style="font-size: 0.8rem"><b style="text-align: center">' + tournament.date + ' - ' + tournament.epreuve + '</b></span><hr></div>')
+          tournament.matchs.forEach((match) => {
+
+            const color = match.vdf == 0 ? "#69BB82" : "#EF736D" // green : red
+            const color_letter = match.vdf == 0 ? "V" : "D"
+            const color_score = match.ex == 0 ? "#433A46" : color;  // black : green/red
+
+            const match_div = $('<div style="display: flex; align-items: center; margin-bottom: 5px;"></div>');
+            const match_icon = $('<div style="margin-right: 8px; font-size: 1.3rem; color: white; text-align: center; background-color: ' + color + '; border-radius: 50%; width: 2em; height: 2em; line-height: 2em;">' + color_letter + '</div>');
+            const match_content = $('<div style="font-size: 0.9rem"><span style="color: #0C9AC1; font-size: 0.9rem">' + match.p  + '</span> - <u style="cursor: pointer">' + match.nom.trim() + '</u><br><span style="color: gray; font-size: 0.9rem; margin-left: auto;">Coef' + (block.processed == 0 ? ' estimé': '') + ': ' + match.coeff + '</span></div>');
+            match_content.find('u').click(function() {
+              // reset match panel
+              $('#matchs').html('');
+              updatePlayerData(match.licence);
+            });
+
+            const match_ex = $('<div style="display: flex; align-items: center; margin-left: auto;"></div>');
+            if (block.processed == 0) {
+              const crown = $('<div style="align-items: center; margin-right: 5px;"><span style="font-size: 1.2rem;">&#x1F451;</span></div>');
+              match_ex.append(crown);
+            }
+            match_ex.append($('<div style="background-color: ' + color_score + '; border-radius: 15%; line-height: 25px; font-weight: bold; width: 39px; height: 26px; text-align: center; font-size: 0.8rem; color: white;">' + match.ex + '</div>'));
+
+            match_div.append(match_icon);
+            match_div.append(match_content);
+            match_div.append(match_ex);
+            colSm.append(match_div);
+
+          });
+          matchs.append(colSm);
+
+          rowDiv.append(colSm);
+          colCount++;
+        });
+
+      });
+      // result.show();
+    }
+  });
+}
+
+
 function search() {
     const input = $('#search-input');
     const value = input.val();
@@ -25,79 +108,7 @@ function search() {
             input.val("");
             suggestions.hide();
             const license = player.license
-            $.ajax({
-              url: "/api/player",
-              data: {
-                license_number: license,
-              },
-              type: "GET",
-              success: function(selected_player) {
-                const result_html = $('#result');
-                for (var prop in selected_player) {
-                    if (Object.prototype.hasOwnProperty.call(selected_player, prop)) {
-                      $('#result_' + prop).html(selected_player[prop]);
-                    }
-                }
-                // result.html(JSON.stringify(selected_player, null, 2));
-                result.show();
-              }
-            });
-            $.ajax({
-              url: "/api/matchs",
-              data: {
-                license_number: license,
-              },
-              type: "GET",
-              success: function(matchs_player) {
-
-                const matchs = $('#matchs');
-                let colCount = 0;
-                let rowDiv;
-
-                matchs_player.list.forEach((block, index_block) => {
-
-                  block.journees.forEach((tournament) => {
-
-                    // create a new row div after every 2 col-sm
-                    if (colCount % 2 == 0) {
-                      rowDiv = $('<div class="row"></div>');
-                      matchs.append(rowDiv);
-                    }
-
-                    const colSm = $('<div class="col-sm-6"><hr><span style="font-size: 0.8rem"><b style="text-align: center">' + tournament.date + ' - ' + tournament.epreuve + '</b></span><hr></div>')
-                    tournament.matchs.forEach((match) => {
-
-                      const color = match.vdf == 0 ? "#69BB82" : "#EF736D" // green : red
-                      const color_letter = match.vdf == 0 ? "V" : "D"
-                      const color_score = match.ex == 0 ? "#433A46" : color;  // black : green/red
-
-                      const match_div = $('<div style="display: flex; align-items: center; margin-bottom: 5px;"></div>');
-                      const match_icon = $('<div style="margin-right: 8px; font-size: 1.3rem; color: white; text-align: center; background-color: ' + color + '; border-radius: 50%; width: 2em; height: 2em; line-height: 2em;">' + color_letter + '</div>');
-                      const match_content = $('<div style="font-size: 0.9rem"><span style="color: #0C9AC1; font-size: 0.9rem">' + match.p  + '</span> - ' + match.nom.trim() + '<br><span style="color: gray; font-size: 0.9rem; margin-left: auto;">Coef' + (block.processed == 0 ? ' estimé': '') + ': ' + match.coeff + '</span></div>');
-
-                      const match_ex = $('<div style="display: flex; align-items: center; margin-left: auto;"></div>');
-                      if (block.processed == 0) {
-                        const crown = $('<div style="align-items: center; margin-right: 5px;"><span style="font-size: 1.2rem;">&#x1F451;</span></div>');
-                        match_ex.append(crown);
-                      }
-                      match_ex.append($('<div style="background-color: ' + color_score + '; border-radius: 15%; line-height: 25px; font-weight: bold; width: 39px; height: 26px; text-align: center; font-size: 0.8rem; color: white;">' + match.ex + '</div>'));
-
-                      match_div.append(match_icon);
-                      match_div.append(match_content);
-                      match_div.append(match_ex);
-                      colSm.append(match_div);
-
-                    });
-                    matchs.append(colSm);
-
-                    rowDiv.append(colSm);
-                    colCount++;
-                  });
-
-                });
-                // result.show();
-              }
-            });
+            updatePlayerData(license);
           });
           suggestions.append(div);
         }
